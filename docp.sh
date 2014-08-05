4974dfe35af252c6f4cdc669d14e62b8341a6078
darraghog <darraghog@Macintosh.local>
2014-08-04 20:30:17 -0400

    Move classes and lib to bin directory

--- docp.sh
+++ docp.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #pushd $HOME/localdev/fancyfoods.dog
 LOCAL=`pwd`
+LIBDIR=$LOCAL/bin/lib
 TARGET=$HOME/localdev/aries/samples-1.0.0/blog/blog-assembly/target
 if ! test -d $TARGET; then 
 	echo "*** ERROR: $TARGET does not exist, install Aries"; 
 else
- 	if ! test -d $LOCAL/lib; then 
+ 	if ! test -d $LIBDIR; then 
 		echo "*** ERROR: lib directory does not exist."; 
 	else
-		cp $LOCAL/lib/* $TARGET/load
+		cp $LIBDIR/* $TARGET/load
 	fi
 fi
 #popd
--- dojar.sh
+++ dojar.sh
@@ -1,39 +1,43 @@
 #!/bin/sh
 #export LOCAL=$HOME/localdev/fancyfoods.dog
 export LOCAL=`pwd`
-if ! test -d $LOCAL/lib; then mkdir $LOCAL/lib; fi
-pushd lib
+export CLASSDIR=$LOCAL/bin/classes
+export LIBDIR=$LOCAL/bin/lib
+if ! test -d $LOCAL/bin; then 
+	echo "No bin directory!"
+	exit
+fi
+if ! test -d $LIBDIR; then mkdir $LIBDIR; fi
 echo "***** Web"
-pushd $LOCAL/classes
-jar cmfv $LOCAL/src/fancyfoods/web/MANIFEST.MF $LOCAL/lib/fancyfoods.web_1.0.0.jar fancyfoods/web  -C $LOCAL/src/fancyfoods/web WEB-INF
+pushd $LIBDIR
+jar cmfv $LOCAL/src/fancyfoods/web/MANIFEST.MF $LIBDIR/fancyfoods.web_1.0.0.jar fancyfoods/web  -C $LOCAL/src/fancyfoods/web WEB-INF
 popd
 echo "****** Web NLS"
 pushd $LOCAL/src/fancyfoods/web/nls
-jar cmfv $LOCAL/src/fancyfoods/web/nls/MANIFEST.MF $LOCAL/lib/fancyfoods.web.nls.fr_1.0.0.jar messages_en_US.properties messages_fr.properties
+jar cmfv $LOCAL/src/fancyfoods/web/nls/MANIFEST.MF $LIBDIR/fancyfoods.web.nls.fr_1.0.0.jar messages_en_US.properties messages_fr.properties
 popd
 echo "****** API"
-pushd $LOCAL/classes
-jar cmfv $LOCAL/src/fancyfoods/api/MANIFEST.MF $LOCAL/lib/fancyfoods.api_1.0.0.jar fancyfoods/api
+pushd $CLASSDIR
+jar cmfv $LOCAL/src/fancyfoods/api/MANIFEST.MF $LIBDIR/fancyfoods.api_1.0.0.jar fancyfoods/api
 popd
 echo "****** Business"
-pushd $LOCAL/classes
-jar cmfv $LOCAL/src/fancyfoods/business/MANIFEST.MF $LOCAL/lib/fancyfoods.business_1.0.0.jar fancyfoods/business -C $LOCAL/src/fancyfoods/business OSGI-INF
+pushd $CLASSDIR
+jar cmfv $LOCAL/src/fancyfoods/business/MANIFEST.MF $LIBDIR/fancyfoods.business_1.0.0.jar fancyfoods/business -C $LOCAL/src/fancyfoods/business OSGI-INF
 popd
 echo "****** Chocolate"
-pushd $LOCAL/classes
-jar cmfv $LOCAL/src/fancyfoods/department/chocolate/MANIFEST.MF $LOCAL/lib/fancyfoods.department.chocolate_1.0.0.jar fancyfoods/department/chocolate -C $LOCAL/src/fancyfoods/department/chocolate OSGI-INF
+pushd $CLASSDIR
+jar cmfv $LOCAL/src/fancyfoods/department/chocolate/MANIFEST.MF $LIBDIR/fancyfoods.department.chocolate_1.0.0.jar fancyfoods/department/chocolate -C $LOCAL/src/fancyfoods/department/chocolate OSGI-INF
 popd
 echo "******* Cheese"
-pushd $LOCAL/classes
-jar cmfv $LOCAL/src/fancyfoods/department/cheese/MANIFEST.MF $LOCAL/lib/fancyfoods.department.cheese_1.0.0.jar   fancyfoods/department/cheese -C $LOCAL/src/fancyfoods/department/cheese OSGI-INF
+pushd $CLASSDIR
+jar cmfv $LOCAL/src/fancyfoods/department/cheese/MANIFEST.MF $LIBDIR/fancyfoods.department.cheese_1.0.0.jar   fancyfoods/department/cheese -C $LOCAL/src/fancyfoods/department/cheese OSGI-INF
 popd
 echo "******* Datasource"
-pushd $LOCAL/classes
-jar cmfv $LOCAL/src/fancyfoods/datasource/MANIFEST.MF $LOCAL/lib/fancyfoods.datasource_1.0.0.jar -C $LOCAL/src/fancyfoods/datasource OSGI-INF
+pushd $CLASSDIR
+jar cmfv $LOCAL/src/fancyfoods/datasource/MANIFEST.MF $LIBDIR/fancyfoods.datasource_1.0.0.jar -C $LOCAL/src/fancyfoods/datasource OSGI-INF
 popd
 echo "******* Persistence"
-pushd $LOCAL/classes
-jar cmfv $LOCAL/src/fancyfoods/persistence/MANIFEST.MF $LOCAL/lib/fancyfoods.persistence_1.0.0.jar  fancyfoods/persistence -C $LOCAL/src/fancyfoods/persistence OSGI-INF  -C $LOCAL/src/fancyfoods/persistence persistence.xml
+pushd $CLASSDIR
+jar cmfv $LOCAL/src/fancyfoods/persistence/MANIFEST.MF $LIBDIR/fancyfoods.persistence_1.0.0.jar  fancyfoods/persistence -C $LOCAL/src/fancyfoods/persistence OSGI-INF  -C $LOCAL/src/fancyfoods/persistence persistence.xml
 popd
 echo "******** Done"
-popd
